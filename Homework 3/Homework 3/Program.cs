a310241715f1a571897b6f6bb0dc884da4ecfcca
ismayilras@code.edu.az <ismayilras@code.edu.az>
2022-12-11 00:43:08 +0400

    Third

---
+++ Homework 3/Homework 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            var today = DateTime.Today;
+            DateTime birthdate = new DateTime(1990, 2, 28);
+            var age=today.Year-birthdate.Year;
+            Console.WriteLine("Yash:"+age);          
+        }
+    }
+}
